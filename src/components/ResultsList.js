d9a3a912f474ec28fd4d588b7fef7cf7d38fff54
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-28 20:47:26 +0100

    results list component

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,20 +1,53 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity
+} from 'react-native';
+import { withNavigation } from 'react-navigation';
+import ResultsDetail from './ResultsDetail';
 
-const ResultsList = ({title, results}) => {
-    return (
-        <View>
-            <Text style={styles.title}>{title}</Text>
-            <Text>Results: {results.length}</Text>
-        </View>
-    );
+const ResultsList = ({ title, results, navigation }) => {
+  if (!results.length) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={results}
+        keyExtractor={result => result.id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate('ResultsShow', { id: item.id })
+              }
+            >
+              <ResultsDetail result={item} />
+            </TouchableOpacity>
+          );
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    title: {
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 5
+  },
+  container: {
+    marginBottom: 10
+  }
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
